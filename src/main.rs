79c2412d56571edbe13c60fcc5769aab267d3d51
Francesco Vennari <dartescouser@gmail.com>
2021-10-31 13:18:53 +0100

    first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+#[derive(Debug)]
+enum Operation {
+    OpPush,
+    OpPlus,
+    OpMinus,
+    OpDump,
+}
+
+fn push(val: u64) -> (Operation, Option<u64>) {
+    (Operation::OpPush, Some(val))
+}
+
+fn plus() -> (Operation, Option<u64>) {
+    (Operation::OpPlus, None)
+}
+
+fn minus() -> (Operation, Option<u64>) {
+    (Operation::OpMinus, None)
+}
+
+fn dump() -> (Operation, Option<u64>) {
+    (Operation::OpDump, None)
+}
+
+fn simulate_program(program: Vec<(Operation, Option<u64>)>) {
+    let mut stack: Vec<u64> = Vec::new();
+
+    for op in program.iter() {
+        match op.0 {
+            Operation::OpPush => if let Some(x) = op.1 {
+                stack.push(x);
+            }
+            Operation::OpPlus => {
+                let a = stack.pop();
+                let b = stack.pop();
+                if let (Some(x), Some(y)) = (a, b) {
+                    stack.push(x + y);
+                }
+            }
+            Operation::OpMinus => {
+                let a = stack.pop();
+                let b = stack.pop();
+                if let (Some(x), Some(y)) = (a, b) {
+                    stack.push(y - x);
+                }
+            }
+            Operation::OpDump => {
+                let a = stack.pop();
+                if let Some(x) = a {
+                    println!("{}", x);
+                }
+            },
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let program = vec![push(2), push(4), plus(), dump(), push(2), push(1), minus(), dump()];
+    simulate_program(program);
 }
